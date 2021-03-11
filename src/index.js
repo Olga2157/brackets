6b397c5814984d523c0986187a07b21ab6130009
Olga Golyakova <olgagolyakova@Olgas-MacBook-Air.local>
2021-03-11 23:09:15 +0300

    init: start brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,40 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  let closedToOpen = new Map();
+  let stack = [];
+  // initialise close to open bracket map
+  for (let config of bracketsConfig) {
+      closedToOpen.set(config[1], config[0]);
+  }
+  for (let i=0; i<str.length; i++) {
+      let curBracket = str[i];
+      // open bracket === closed bracket
+      if (closedToOpen.get(curBracket) === curBracket) {
+          if (curBracket === stack[stack.length - 1]) {
+              // everything is fine, remove last elem and go to the next elem
+              stack.pop();
+          } else {
+              // put open bracket to the stack
+              stack.push(curBracket);
+          }
+      }
+      // if bracket is open bracket
+      else if (!closedToOpen.has(curBracket)) {
+          // put open bracket to the stack
+          stack.push(curBracket);
+      } else {
+          if (closedToOpen.get(curBracket) === stack[stack.length - 1]) {
+              // everything is fine, remove last elem and go to the next elem
+              stack.pop();
+          } else {
+              // last bracket(closed bracket) is not closing last open bracket
+              return false;
+          }
+      }
+  }
+  if (stack.length === 0) {
+      return true;
+  } else {
+      return false;
+  }
+  // return stack.length === 0;
+};
